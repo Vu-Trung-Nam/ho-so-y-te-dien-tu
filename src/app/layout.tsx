98e57ee61722ec00cc30b31d1198d0185236a18c
VuTrungNam <vutrungnamk13@siu.edu.vn>
2025-03-13 20:45:20 +0700

    move icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
 
-        <link rel="icon" href="/img/new-favicon.ico" />
+        <link rel="icon" href="/new-favicon.ico" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
